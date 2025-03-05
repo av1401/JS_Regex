164173cf64daafc9c85fe209dc9b0fcbabcaed5e
av1401 <rsabhi1401@gmail.com>
2025-03-05 13:02:09 +0530

    [Abhishek] add Ensure '.' and Validation

--- Email/Email.js
+++ Email/Email.js
@@ -8,3 +8,5 @@ function validateEmail(email) {
 console.log(validateEmail("abc@bridgelabz.co"));
 console.log(validateEmail("abc.xyz@bridgelabz.co.in"));
 console.log(validateEmail("abc@bridgelabz.com"));
+console.log(validateEmail("xyz@bridgelabz.co"));//UC3
+console.log(validateEmail("abc@xyz.co"));//UC3
