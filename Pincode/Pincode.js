a7e80a47f98b33e7eca6fb41f9456de5e532eecc
av1401 <rsabhi1401@gmail.com>
2025-03-05 13:38:11 +0530

    [Abhishek] add. Check for 400 088 and 400088

--- Pincode/Pincode.js
+++ Pincode/Pincode.js
@@ -1,4 +1,4 @@
-const pinCodeRegex = /^[1-9][0-9]{5}$/;
+const pinCodeRegex = /^[1-9][0-9]{2}\s?[0-9]{3}$/;
 
 function validatePinCode(pinCode) {
     return pinCodeRegex.test(pinCode);
@@ -9,3 +9,4 @@ console.log(validatePinCode("400088")); //UC1
 console.log(validatePinCode("000088")); //UC1
 console.log(validatePinCode("A400088")); //UC2
 console.log(validatePinCode("400088B")); //UC3
+console.log(validatePinCode("400 088")); //UC4
